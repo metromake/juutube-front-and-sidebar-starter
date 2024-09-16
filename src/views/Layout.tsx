97ee318e9d18cedacb497598e42ecd116bd2dfec
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-09-17 00:40:41 +0300

    update

---
+++ src/views/Layout.tsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <header>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sidebar">Sidebar</Link>
+          </li>
+        </ul>
+      </header>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
